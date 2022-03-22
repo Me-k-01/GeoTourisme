e4a7d9c2031951484cd5502fcf1c9a4564ba1b48
Nitoute <hamza-chabl@hotmail.com>
2022-03-22 16:20:28 +0100

    ajout serveur récupération base de donnée

---
+++ serveur/main.py
@@ -0,0 +1,31 @@
+from contextlib import nullcontext
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+from flaskext.mysql import MySQL
+
+
+app = Flask(__name__)
+api = Api(app)
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_HOST'] = '5.189.183.247'
+app.config['MYSQL_DATABASE_USER'] = 'geotourisme'
+app.config['MYSQL_DATABASE_PASSWORD'] = '7`Yxv<{_e*&/{QZm'
+app.config['MYSQL_DATABASE_DB'] = 'geotourisme'
+
+mysql.init_app(app)
+
+class recuperation(Resource):
+    def get(self):
+        iduser = request.args.get('id')
+        connect = mysql.get_db()
+        cursor = connect.cursor()
+        cursor.execute("SELECT * from Users where id='") + iduser + ("'")
+        data = cursor.fetchone() 
+        print("data :"+data)
+        return data
+
+
+api.add_resource(recuperation, '/connect')
+if __name__ == '__main__':
+    app.run(debug=True)

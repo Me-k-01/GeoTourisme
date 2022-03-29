afcfc4b01a83a7335d59d9ad740f2e1262f80781
Me-k-01 <mecaman01@gmail.com>
2022-03-29 14:01:05 +0200

    Extrusion des buildings sur la carte

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -7,28 +7,63 @@ import mapboxgl from "mapbox-gl"
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN!
 
 export const Map = () => {
-  const mapContainer = useRef(null)
+    const mapContainer = useRef(null)
 
-  // this is where all of our map logic is going to live
-  // adding the empty dependency array ensures that the map
-  // is only created once
-  useEffect(() => {
-    // create the map and configure it
-    // check out the API reference for more options
-    // https://docs.mapbox.com/mapbox-gl-js/api/map/
-    const map = new mapboxgl.Map({
-      container: "map",
-      style: "mapbox://styles/mapbox/satellite-streets-v11",
-      center: [-119.99959421984575, 38.619551620333496],
-      zoom: 14,
-    })
-  }, [])
 
-  return (
-    <div
-      id="map"
-      ref={mapContainer}
-      style={{ width: "100%", height: "100vh" }}
-    />
-  )
+    useEffect(() => {
+        // API reference: https://docs.mapbox.com/mapbox-gl-js/api/map/
+        const map = new mapboxgl.Map({
+            container: "map",
+            style: "mapbox://styles/mapbox/outdoors-v11",
+            center: [2.146400, 43.928902],      
+            zoom: 12,      
+            pitch: 60,      
+            //bearing: 80, // Orientation
+        });
+        // On modifie les couches qu'après que la map s'est completement chargée
+        map.on("load", () => 
+            map.addLayer({
+                id: "add-3d-buildings",
+                source: "composite",
+                "source-layer": "building",
+                filter: ["==", "extrude", "true"],
+                type: "fill-extrusion",
+                minzoom: 15,
+                paint: {
+                    "fill-extrusion-color": "#aaa",
+    
+                    // Use an "interpolate" expression to
+                    // add a smooth transition effect to
+                    // the buildings as the user zooms in.
+                    "fill-extrusion-height": [
+                        "interpolate",
+                        ["linear"],
+                        ["zoom"],
+                        15,
+                        0,
+                        15.05,
+                        ["get", "height"]
+                    ],
+                    "fill-extrusion-base": [
+                        "interpolate",
+                        ["linear"],
+                        ["zoom"],
+                        15,
+                        0,
+                        15.05,
+                        ["get", "min_height"]
+                    ],
+                    "fill-extrusion-opacity": 0.6
+                }
+            });
+        });
+    }, []);
+
+    return (
+        <div
+            id="map"
+            ref={mapContainer}
+            style={{ width: "100%", height: "100vh" }}
+        />
+    )
 }

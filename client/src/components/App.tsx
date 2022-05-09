9d835feb8939ccd028f56344707d97338a07ce58
Me-k-01 <mecaman01@gmail.com>
2022-05-09 13:59:12 +0200

    Changement affichage du resultat

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,51 +1,31 @@
-import {Map} from "./Map"
-import Search from "./Search"
+import { useState } from "react";
+import { Map } from "./Map"
+import {Search, Adresse} from "./Search"
 
 const axios = require('axios');
 
 function App() {
+  const [searchResultat, setSearchResultat] = useState<Adresse[]>([]);
+  const [hideResult, setHideResult] = useState(true);
+
   return (
     <div>
       <header className="fixed-top">
-        <Search onSubmit={(str) => {
+        <Search hideResult={hideResult} onSubmit={(str) => {
+          setHideResult(false);
+
           try {
-            var res = document.getElementById("result");
-            if((str === "") && (res != null)) res.hidden = true;
-            else {
-              const result = axios.get(`/contains/${str}`);
-              result.then((resp : any) => {
-                if(res != null) {
-                  res.innerHTML = '';
-                  res.hidden = false;
-                  if(resp.data.length == 0) {
-                    var div = document.createElement("div");
-                    div.innerHTML = "Aucun résultat :/";
-                    res.appendChild(div);
-                  }
-                  else
-                    for(let k in resp.data) {
-                      const div = document.createElement("div");
-                      div.classList.add("select");
-                      div.setAttribute("lat", resp.data[k][2]);
-                      div.setAttribute("long", resp.data[k][3]);
-                      const h4 = document.createElement("h4");
-                      h4.innerHTML = resp.data[k][0];
-                      const p = document.createElement("p");
-                      p.innerHTML = resp.data[k][1];
-                      div.appendChild(h4);
-                      div.appendChild(p);
-                      div.addEventListener('click', function(e) {
-                          console.log(div.getAttribute("lat") + " " + div.getAttribute("long"));
-                      });
-                      res.appendChild(div);
-                    }
-                }
+            if (str === "")
+              setSearchResultat([]);
+            else 
+              axios.get(`/contains/${str}`).then((resp: any) => {
+                console.log(resp.data);
+                setSearchResultat(resp.data);
               });
-            }
-          } catch (err){
+          } catch (err) {
             console.log(err);
           }
-        }} />
+        }} resultat={searchResultat} />
       </header>
       <Map />
       <div>

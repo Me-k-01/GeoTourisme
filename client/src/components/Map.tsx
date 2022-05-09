2e9db3cb7027db84a2e2100061b996926a54fa41
DJVince81 <vincent.rodriguez81640@gmail.com>
2022-05-09 18:03:46 +0200

    Ajout de le géolocalisation du l'utilisateur

--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,58 +1,125 @@
 import { useRef, useEffect } from "react"
 import mapboxgl  from "mapbox-gl"
+var turf = require('@turf/turf');
 // Accès du token dans le fichier .en.local
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN!
 
 export const Map = () => {
-    const mapContainer = useRef(null)
+    const mapContainer = useRef(null);
 
+    var loc = {lat: 2.146400,
+               long: 43.928902};
+
+    function success(pos: any) {
+        console.log(pos);
+        loc.lat = pos.coords.latitude;
+        loc.long = pos.coords.longitude;
+    }
+    function error() {}
+
+    if(!navigator.geolocation) {
+        //Géolocalisation non supportée
+    } else {
+        navigator.geolocation.getCurrentPosition(success, error, {enableHighAccuracy: true,
+                                                                  timeout: 5000,
+                                                                  maximumAge: 0});
+    }
+
+    // Create a GeoJSON feature collection for drop-off locations
+    const dropoffs = turf.featureCollection(turf.point([loc.lat, loc.long]));
+    console.log(dropoffs);
 
     useEffect(() => {
         // réference d'API: https://docs.mapbox.com/mapbox-gl-js/api/map/
         const map = new mapboxgl.Map({
             container: "map",
             style: "mapbox://styles/mapbox/streets-v11",
-            center: [2.146400, 43.928902],
+            center: [loc.lat, loc.long],
             zoom: 12,
             pitch: 60,
             //bearing: 80, // Orientation
         });
         // On modifie les couches qu'après que la map s'est completement chargée
-        map.on("load", () =>
-            map.addLayer({
-                id: "add-3d-buildings",
-                source: "composite",
-                "source-layer": "building",
-                filter: ["==", "extrude", "true"],
-                type: "fill-extrusion",
-                minzoom: 15,
-                paint: {
-                    "fill-extrusion-color": "#aaa",
+        map.on("load", async () => {
+                map.addLayer({
+                    id: "add-3d-buildings",
+                    source: "composite",
+                    "source-layer": "building",
+                    filter: ["==", "extrude", "true"],
+                    type: "fill-extrusion",
+                    minzoom: 15,
+                    paint: {
+                        "fill-extrusion-color": "#aaa",
 
-                    // Use an "interpolate" expression to
-                    // add a smooth transition effect to
-                    // the buildings as the user zooms in.
-                    "fill-extrusion-height": [
-                        "interpolate",
-                        ["linear"],
-                        ["zoom"],
-                        15,
-                        0,
-                        15.05,
-                        ["get", "height"]
-                    ],
-                    "fill-extrusion-base": [
-                        "interpolate",
-                        ["linear"],
-                        ["zoom"],
-                        15,
-                        0,
-                        15.05,
-                        ["get", "min_height"]
-                    ],
-                    "fill-extrusion-opacity": 0.6
-                }
-            })
+                        // Use an "interpolate" expression to
+                        // add a smooth transition effect to
+                        // the buildings as the user zooms in.
+                        "fill-extrusion-height": [
+                            "interpolate",
+                            ["linear"],
+                            ["zoom"],
+                            15,
+                            0,
+                            15.05,
+                            ["get", "height"]
+                        ],
+                        "fill-extrusion-base": [
+                            "interpolate",
+                            ["linear"],
+                            ["zoom"],
+                            15,
+                            0,
+                            15.05,
+                            ["get", "min_height"]
+                        ],
+                        "fill-extrusion-opacity": 0.6
+                    }
+                });
+
+                const marker = document.createElement('div');
+                marker.classList.add('truck');
+
+                // Create a new marker
+                new mapboxgl.Marker(marker).setLngLat([loc.lat, loc.long]).addTo(map);
+                                
+                map.addLayer({
+                    id: 'warehouse-symbol',
+                    type: 'symbol',
+                    source: {
+                        data: dropoffs,
+                        type: 'geojson'
+                    },
+                    layout: {
+                        'icon-image': 'grocery-15',
+                        'icon-size': 1
+                    },
+                    paint: {
+                        'text-color': '#3887be'
+                    }
+                });
+                /*
+                map.addLayer(
+                {
+                    id: 'routearrows',
+                    type: 'symbol',
+                    source: 'route',
+                    layout: {
+                        'symbol-placement': 'line',
+                        'text-field': '▶',
+                        'text-size': ['interpolate', ['linear'], ['zoom'], 12, 24, 22, 60],
+                        'symbol-spacing': ['interpolate', ['linear'], ['zoom'], 12, 30, 22, 160],
+                        'text-keep-upright': false
+                    },
+                    paint: {
+                        'text-color': '#3887be',
+                        'text-halo-color': 'hsl(55, 11%, 96%)',
+                        'text-halo-width': 3
+                    }
+                  },
+                  'waterway-label'
+                );*/
+                return map;
+            }
         );
 
 

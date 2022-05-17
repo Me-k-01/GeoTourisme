702f86cb13ad79b5ab8c83a380e6c43ccd1b1f26
Me-k-01 <mecaman01@gmail.com>
2022-05-17 20:35:19 +0200

    Correction traçage du chemin quand on clique sur la map

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -6,58 +6,64 @@ import { Scroller } from "./Scroller";
 import { Adresse } from "../Adresse";
 
 function App() {
-    const [searchResult, setSearchResult] = useState<Adresse[]>([]);
-    const [hideResult, setHideResult] = useState(true);
-    const [markers, setMarkers] = useState<Location[]>([]);
-    const [markersSecondaires, setMarkersSecondaires] = useState<Location[]>([]);
-    const [showMenu, setShowMenu] = useState(true);
+  const [searchResult, setSearchResult] = useState<Adresse[]>([]);
+  const [hideResult, setHideResult] = useState(true);
+  const [markers, setMarkers] = useState<Location[]>([]);
+  const [markersSecondaires, setMarkersSecondaires] = useState<Location[]>([]);
+  const [showMenu, setShowMenu] = useState(true);
 
-    const whenHidden = (className: string) => showMenu ? "" : "  " + className;
+  const whenHidden = (className: string) => showMenu ? "" : "  " + className;
 
-    return (
-        <>
-            <aside className={"side-interface" + whenHidden("closed")}>
-                <Search onSubmit={(str) => {
-                    setHideResult(false);
+  return (
+    <>
+      <aside className={"side-interface" + whenHidden("closed")}>
+        <Search onSubmit={(str) => {
+          setHideResult(false);
 
-                    try {
-                        if (str === "")
-                            setSearchResult([]);
-                        else
-                            axios.get(`/contains/${str}`).then((resp: any) => {
-                                setSearchResult(resp.data);
-                            });
-                    } catch (err) {
-                        console.log(err);
-                    }
-                }} />
-                {!hideResult && <Scroller list={searchResult} onSelect={(loc) => {
-                    // Verifie si le marqueur correspond au marqueur courrant
-                    const cond = ({ long, lat }: Location) => long === loc.long && lat === loc.lat;
+          try {
+            if (str === "")
+              setSearchResult([]);
+            else
+              axios.get(`/contains/${str}`).then((resp: any) => {
+                setSearchResult(resp.data);
+              });
+          } catch (err) {
+            console.log(err);
+          }
+        }} />
+        {!hideResult && <Scroller list={searchResult} onSelect={(loc) => {
 
-                    if (!markers.find(cond)) // Ajout du marqueur
-                        setMarkers(markers => [...markers, loc]);
-                    else // retirer le marqueur
-                        setMarkers(markers.filter(loc => !cond(loc)));
-                }} />}
-            </aside>
-            <div className="expander-wrapper">
-                <button className="expander" onClick={() => {
-                    setShowMenu(!showMenu);
-                    const interval = setInterval(() => {
-                        window.dispatchEvent(new Event('resize'))
-                    }, 10)
-                    setTimeout(() => {
-                        clearTimeout(interval);
-                    }, 500);
-                }}>
-                    <i className={"fa-solid fa-caret-left" + whenHidden("rotate")} />
-                </button>
+          // s'il y a trop de marqueur
+          if (markers.length + markersSecondaires.length > 10)
+            return alert(`Votre parcours comporte trop de lieux.\nLimitez vos choix pour en profiter.`);
 
-            </div>
-            <Map markers={markers} setMarkers={setMarkers} markersSecondaires={markersSecondaires} setMarkersSecondaires={setMarkersSecondaires} />
-        </>
-    );
+          // Fonction pour vérifier si le marqueur correspond au marqueur courrant
+          const cond = ({ long, lat }: Location) => long === loc.long && lat === loc.lat;
+
+          // Si le marqueur n'a pas déjà été créé  
+          if (!markers.find(cond)) // On ajoute le nouveau marqueur
+            setMarkers(markers => [...markers, loc]);
+          else // Retirer le marqueur
+            setMarkers(markers.filter(loc => !cond(loc)));
+        }} />}
+      </aside>
+      <div className="expander-wrapper">
+        <button className="expander" onClick={() => {
+          setShowMenu(!showMenu);
+          const interval = setInterval(() => {
+            window.dispatchEvent(new Event('resize'))
+          }, 10)
+          setTimeout(() => {
+            clearTimeout(interval);
+          }, 500);
+        }}>
+          <i className={"fa-solid fa-caret-left" + whenHidden("rotate")} />
+        </button>
+
+      </div>
+      <Map markers={markers} setMarkers={setMarkers} markersSecondaires={markersSecondaires} setMarkersSecondaires={setMarkersSecondaires} />
+    </>
+  );
 }
 
 export default App;
--- client/src/components/Map.tsx
+++ client/src/components/Map.tsx
@@ -1,65 +1,61 @@
-import { wait } from "@testing-library/user-event/dist/utils";
 import * as turf from "@turf/turf"
 import { FC, useEffect, useState } from "react"
 import { Map as MapBox, Layer, Marker, Source } from 'react-map-gl';
-import App from "./App";
 
 export type Location = {
-    lat: number;
-    long: number;
+  lat: number;
+  long: number;
 };
 
 type Geometry = {
-    type: "Point";
-    coordinates: number[];
+  type: "Point";
+  coordinates: number[];
 };
 
 
 interface Trip {
-    code: string;
-    waypoints: any[];
-    trips: {
-        geometry: Geometry;
-    }[];
+  code: string;
+  waypoints: any[];
+  trips: {
+    geometry: Geometry;
+  }[];
 }
 
 interface IMapProp {
-    markers: Location[],
-    markersSecondaires: Location[],
-    setMarkers: Function,
-    setMarkersSecondaires: Function
+  markers: Location[],
+  markersSecondaires: Location[],
+  setMarkers: React.Dispatch<React.SetStateAction<Location[]>>,
+  setMarkersSecondaires: React.Dispatch<React.SetStateAction<Location[]>>
 }
 export const Map: FC<IMapProp> = ({ markers, markersSecondaires, setMarkers, setMarkersSecondaires }) => {
-    const allMarkers = markers.concat(markersSecondaires);
+  const [pos, setPos] = useState<Location>({ // Position par défaut: Albi
+    lat: 43.928902,
+    long: 2.146400
+  });
 
-    const [pos, setPos] = useState<Location>({
-        lat: 43.928902,
-        long: 2.146400
-    });
+  const [route, setRoute] = useState<turf.FeatureCollection<Geometry, {}>>(turf.featureCollection([]));
+  const [editRoute, setEditRoute] = useState<boolean>(false);
 
-    const [route, setRoute] = useState<turf.FeatureCollection<Geometry, {}>>(turf.featureCollection([]));
-    const [editRoute, setEditRoute] = useState<boolean>(false);
-
-    useEffect(() => {
-        ///////// Géolocalisation /////////
-        if (!navigator.geolocation)
-            console.error("Géolocalisation non supportée");
-        else
-            setInterval(() => {
-                navigator.geolocation.getCurrentPosition((pos) => {
-                    // console.log("Votre position actuelle:", pos);
-                    setPos({
-                        lat: pos.coords.latitude,
-                        long: pos.coords.longitude
-                    });
-                }, null, {
-                    enableHighAccuracy: true,
-                    timeout: 5000,
-                    maximumAge: 0
-                });
-            }, 5000);
-        ///////////////////////////////
-    }, []);
+  useEffect(() => {
+    ///////// Géolocalisation /////////
+    if (!navigator.geolocation)
+      console.error("Géolocalisation non supportée");
+    else
+      setInterval(() => {
+        navigator.geolocation.getCurrentPosition((pos) => {
+          // console.log("Votre position actuelle:", pos);
+          setPos({
+            lat: pos.coords.latitude,
+            long: pos.coords.longitude
+          });
+        }, null, {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 0
+        });
+      }, 5000);
+    ///////////////////////////////
+  }, []);
 
   // const geolocateControlRef = useCallback((ref) => {
   //   if (ref) {
@@ -67,8 +63,15 @@ export const Map: FC<IMapProp> = ({ markers, markersSecondaires, setMarkers, set
   //     ref.trigger();
   //   }
   // }, []);
+  const removeMarker = (markers: Location[], marker: Location) =>
+    markers.filter(({ long, lat }) =>
+      marker.long !== long || marker.lat !== lat
+    );
+
   useEffect(() => {
     const getPath = async (): Promise<Trip | undefined> => {
+      const allMarkers = markers.concat(markersSecondaires);
+
       if (allMarkers.length < 1) {
         setRoute(turf.featureCollection([]));
         return;
@@ -94,125 +97,118 @@ export const Map: FC<IMapProp> = ({ markers, markersSecondaires, setMarkers, set
       setRoute(routeGeoJSON);
     }
     createPath();
-  }, [markers, pos]); // S'il y a un changement de marqueurs ou de position on update le chemin
+  }, [markers, markersSecondaires, pos]); // S'il y a un changement de marqueurs ou de position on update le chemin 
 
-    return <MapBox
-        initialViewState={{
-            longitude: pos.long,
-            latitude: pos.lat,
-            zoom: 12,
-            pitch: 50,
-        }}
-        mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN!} // Accès du token dans le fichier .en.local
-        style={{ flexGrow: 1, width: "100%" }}
-        mapStyle="mapbox://styles/mapbox/streets-v11"
-        onClick={function (e) {
-            e.preventDefault();
-            if (editRoute) {
-                setEditRoute(false);
-                return;
-            }
-            const pos = e.lngLat;
-            const loc = {
-                lat: pos.lat,
-                long: pos.lng
-            }
-            console.log(loc);
-            //Limitation du nombres de points:
-            if (allMarkers.length >= 10) {
-                alert(`Votre parcours comporte trop de lieux.\n
-                Limitez vos choix pour en profiter.`);
-            }
-            else {
-                markersSecondaires.push(loc);
-                setMarkersSecondaires(markersSecondaires);
-            }
-        }}
-    >
-        {/* <GeolocateControl ref={geolocateControlRef} trackUserLocation={true}
+  return <MapBox
+    initialViewState={{
+      longitude: pos.long,
+      latitude: pos.lat,
+      zoom: 12,
+      pitch: 50,
+    }}
+    mapboxAccessToken={process.env.REACT_APP_MAPBOX_TOKEN!} // Accès du token dans le fichier .en.local
+    mapStyle="mapbox://styles/mapbox/streets-v11"
+    onClick={(evt) => {
+      evt.preventDefault();
+      if (editRoute) {
+        setEditRoute(false);
+        return;
+      } 
+      const pos = evt.lngLat;
+      const loc = {
+        lat: pos.lat,
+        long: pos.lng
+      } 
+      //Limitation du nombres de points:
+      if (markers.length + markersSecondaires.length > 10) {
+        alert(`Votre parcours comporte trop de lieux.\nLimitez vos choix pour en profiter.`);
+      } else {
+        setMarkersSecondaires([...markersSecondaires, loc]);
+      }
+    }}
+  >
+    {/* <GeolocateControl ref={geolocateControlRef} trackUserLocation={true}
       onGeolocate={(pos) => {
         console.log("pos:", pos); 
       }}
     /> */}
-        <Layer id="add-3d-buildings" source="composite" source-layer="building"
-            filter={["==", "extrude", "true"]}
-            type="fill-extrusion"
-            minzoom={15}
-            paint={{
-                "fill-extrusion-color": "#aaa",
-                // Use an "interpolate" expression to
-                // add a smooth transition effect to
-                // the buildings as the user zooms in.
-                "fill-extrusion-height": [
-                    "interpolate",
-                    ["linear"],
-                    ["zoom"],
-                    15,
-                    0,
-                    15.05,
-                    ["get", "height"]
-                ],
-                "fill-extrusion-base": [
-                    "interpolate",
-                    ["linear"],
-                    ["zoom"],
-                    15,
-                    0,
-                    15.05,
-                    ["get", "min_height"]
-                ],
-                "fill-extrusion-opacity": 0.6
-            }}
-        />
+    <Layer id="add-3d-buildings" source="composite" source-layer="building"
+      filter={["==", "extrude", "true"]}
+      type="fill-extrusion"
+      minzoom={15}
+      paint={{
+        "fill-extrusion-color": "#aaa",
+        // Use an "interpolate" expression to
+        // add a smooth transition effect to
+        // the buildings as the user zooms in.
+        "fill-extrusion-height": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          15,
+          0,
+          15.05,
+          ["get", "height"]
+        ],
+        "fill-extrusion-base": [
+          "interpolate",
+          ["linear"],
+          ["zoom"],
+          15,
+          0,
+          15.05,
+          ["get", "min_height"]
+        ],
+        "fill-extrusion-opacity": 0.6
+      }}
+    />
 
-        <Source id="route" type="geojson" data={route}>
-            <Layer id='routeline-active' type='line'
-                layout={{
-                    'line-join': 'round',
-                    'line-cap': 'round'
-                }}
-                paint={{
-                    'line-color': '#3887be',
-                    'line-width': ['interpolate', ['linear'], ['zoom'], 12, 3, 22, 12]
-                }}
-            />
-            <Layer id='routearrows' type='symbol'
-                layout={{
-                    'symbol-placement': 'line',
-                    'text-field': '▶',
-                    'text-size': ['interpolate', ['linear'], ['zoom'], 12, 24, 22, 60],
-                    'symbol-spacing': ['interpolate', ['linear'], ['zoom'], 12, 30, 22, 160],
-                    'text-keep-upright': false
-                }}
-                paint={{
-                    'text-color': '#3887be',
-                    'text-halo-color': 'hsl(55, 11%, 96%)',
-                    'text-halo-width': 3
-                }} />
-        </Source>
-        <Marker longitude={pos.long} latitude={pos.lat} color="#dd5555" anchor="center" />
-        {markers.map(({ long, lat }, i) =>
-            <Marker key={i} longitude={long} latitude={lat} anchor="center"
-                onClick={function (e) {
-                    e.originalEvent.preventDefault();
-                    setEditRoute(true);
-                    const cond = (lng: number, lt: number) => lng === long && lt === lat;
-                    setMarkers(markers.filter(loc => !cond(loc.long, loc.lat)));
-                }}
-            >
-                <i className="fa-solid  fa-landmark fa-2xl" ></i>
-            </Marker>
-        )}
-        {markersSecondaires.map(({ long, lat }, i) =>
-            <Marker key={i} longitude={long} latitude={lat} anchor="center"
-                onClick={function (e) {
-                    e.originalEvent.preventDefault();
-                    setEditRoute(true);
-                    const cond = (lng: number, lt: number) => lng === long && lt === lat;
-                    setMarkersSecondaires(markersSecondaires.filter(loc => !cond(loc.long, loc.lat)));
-                }}
-            >
-            </Marker>
-        )}
-    </MapBox>;
+    <Source id="route" type="geojson" data={route}>
+      <Layer id='routeline-active' type='line'
+        layout={{
+          'line-join': 'round',
+          'line-cap': 'round'
+        }}
+        paint={{
+          'line-color': '#3887be',
+          'line-width': ['interpolate', ['linear'], ['zoom'], 12, 3, 22, 12]
+        }}
+      />
+      <Layer id='routearrows' type='symbol'
+        layout={{
+          'symbol-placement': 'line',
+          'text-field': '▶',
+          'text-size': ['interpolate', ['linear'], ['zoom'], 12, 24, 22, 60],
+          'symbol-spacing': ['interpolate', ['linear'], ['zoom'], 12, 30, 22, 160],
+          'text-keep-upright': false
+        }}
+        paint={{
+          'text-color': '#3887be',
+          'text-halo-color': 'hsl(55, 11%, 96%)',
+          'text-halo-width': 3
+        }} />
+    </Source>
+    <Marker longitude={pos.long} latitude={pos.lat} color="#dd5555" anchor="center" />
+    {markers.map((currMarker, i) =>
+      <Marker key={i} longitude={currMarker.long} latitude={currMarker.lat} anchor="center"
+        onClick={(evt) => {
+          evt.originalEvent.preventDefault();
+          setEditRoute(true);
+          setMarkers(removeMarker(markers, currMarker));
+        }}
+      >
+        <i className="fa-solid  fa-landmark fa-2xl" ></i>
+      </Marker>
+    )}
+    {markersSecondaires.map((currMarker, i) =>
+      <Marker key={i} longitude={currMarker.long} latitude={currMarker.lat} anchor="center"
+        onClick={(evt) => {
+          evt.originalEvent.preventDefault();
+          setEditRoute(true);
+          setMarkersSecondaires(removeMarker(markersSecondaires, currMarker));
+        }}
+      >
+      </Marker>
+    )}
+  </MapBox>;
 }

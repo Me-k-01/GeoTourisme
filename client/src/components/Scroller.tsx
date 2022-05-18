412f0720f250deddb865e5c58099102fbc62f1c6
Me-k-01 <mecaman01@gmail.com>
2022-05-18 16:07:11 +0200

    modif scroller

--- client/src/components/Scroller.tsx
+++ client/src/components/Scroller.tsx
@@ -1,8 +1,37 @@
-import { FC } from "react";
+import { FC, MouseEventHandler, useState } from "react";
 import { Address } from "../Address";
 import ReactStars from 'react-stars';
 import axios from 'axios';
- 
+
+interface IDisplayAddressProps {
+    address: Address;
+    className: string;
+    onClick: MouseEventHandler<HTMLLIElement>;
+    key: number;
+};
+
+export const DisplayAddress: FC<IDisplayAddressProps> = ({ key, className, address, onClick }) => {
+    // const [userNote, setUserNote] = useState(address.userNote);
+
+    return (
+        <li key={key} className={className} onClick={onClick}>
+            <h3>{address.nom}</h3>
+            <p>{address.adresse}</p>
+            <div className="star-wrapper" onClick={(evt) => {
+                evt.stopPropagation();
+            }}>
+                <ReactStars count={5} value={address.userNote || Math.trunc(Math.random() * 5) + 1} onChange={(n) => {
+                    console.log("Note ajouté:", n);
+                    const uuid = localStorage.getItem('uuid'); 
+                    axios.post(`/addNote/${address.nom}/${n}/${uuid}`);
+                    // setUserNote(n);
+                }} size={24} color1={'#ccc'} color2={'#ffd700'} />
+            </div>
+        </li>
+    );
+}
+
+
 export interface IScrollerProps {
     list: Address[];
     onSelect: (address: Address, index: number) => void;
@@ -19,25 +48,11 @@ export const Scroller: FC<IScrollerProps> = ({ showPreview, list, onSelect, sele
     return (list.length === 0 ?
         <div>Pas de resultat</div> :
         <ul className="scroller">
-            {list.map((address, i) =>
-                <li key={i} className={getClass(i)} onClick={() => {
+            {list.map((address, i) => {
+                <DisplayAddress key={i} className={getClass(i)} address={address} onClick={() => {
                     onSelect(address, i);
-                }}>
-                    <h3>{address.nom}</h3>
-                    <p>{address.adresse}</p>
-                    <div className="star-wrapper" onClick={(evt) => {
-                        evt.stopPropagation();
-                    }}>
-                        <ReactStars count={5} value={address.note || Math.trunc(Math.random() * 5) + 1} onChange={(n) => {
-                            console.log("Note ajouté:", n);
-                            const uuid = localStorage.getItem("mapbox.eventData.uuid:")
-                            console.log(uuid);
-                            
-                            // TODO
-                            // axios.post(`/addNote/<nom_l>/<note>/<id>`);
-                        }} size={24} color1={'#ccc'} color2={'#ffd700'} />
-                    </div>
-                </li>
+                }} />
+            }
             )}
         </ul>
     );
